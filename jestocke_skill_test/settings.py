76a612c1a6b41208c17e34572be4676d5d101591
mathkora <mathildekora@gmail.com>
2023-11-10 02:41:06 +0100

    Back-end : Add rangefilter app

--- jestocke_skill_test/settings.py
+++ jestocke_skill_test/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rangefilter',
     'djmoney',
     'market_place',
     'booking',
@@ -118,3 +119,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
